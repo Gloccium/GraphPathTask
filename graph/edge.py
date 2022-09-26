6f28b18f7bd1bca271c09b0c6c4e4c90d099f412
Gloccium <kurururunku@gmail.com>
2022-09-26 14:45:04 +0500

    class Edge

--- graph/edge.py
+++ graph/edge.py
@@ -1,3 +1,10 @@
 class Edge:
-    def __init__(self):
-        pass
+    def __init__(self, first_vertex, second_vertex, weight=1):
+        self.first_vertex = first_vertex
+        self.second_vertex = second_vertex
+        self.weight = weight
+
+    def get_opposite_vertex(self, vertex):
+        if self.first_vertex == vertex:
+            return self.second_vertex
+        return self.first_vertex
