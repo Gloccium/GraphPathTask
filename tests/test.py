7659a50d4529e6bf3c99a0aede25f49d28b4454e
Gloccium <kurururunku@gmail.com>
2022-10-04 14:57:58 +0500

    floyd warshall tested

--- tests/test.py
+++ tests/test.py
@@ -48,3 +48,49 @@ class Test(unittest.TestCase):
         with self.assertRaises(ValueError):
             graph.bellman_ford(1)
             graph.bellman_ford(2)
+
+    def test_floyd_warshall(self):
+        graph = Algorithm(vertices=9)
+        graph.connect_vertices(0, 1, 4)
+        graph.connect_vertices(0, 6, 7)
+        graph.connect_vertices(1, 6, 11)
+        graph.connect_vertices(1, 7, 20)
+        graph.connect_vertices(1, 2, 9)
+        graph.connect_vertices(2, 3, 6)
+        graph.connect_vertices(2, 4, 2)
+        graph.connect_vertices(3, 4, 10)
+        graph.connect_vertices(3, 5, 5)
+        graph.connect_vertices(4, 5, 15)
+        graph.connect_vertices(4, 7, 1)
+        graph.connect_vertices(4, 8, 5)
+        graph.connect_vertices(5, 8, 12)
+        graph.connect_vertices(6, 7, 1)
+        graph.connect_vertices(7, 8, 3)
+        self.assertEqual(graph.floyd_warshall(),
+                         [[0, 4, 13, 19, 15, 24, 7, 8, 11],
+                          [float('inf'), 0, 9, 15, 11, 20, 11, 12, 15],
+                          [float('inf'), float('inf'),
+                           0, 6, 2, 11, float('inf'), 3, 6],
+                          [float('inf'), float('inf'), float('inf'),
+                           0, 10, 5, float('inf'), 11, 14],
+                          [float('inf'), float('inf'), float('inf'),
+                           float('inf'), 0, 15, float('inf'), 1, 4],
+                          [float('inf'), float('inf'), float('inf'),
+                           float('inf'), float('inf'), 0, float('inf'),
+                           float('inf'), 12],
+                          [float('inf'), float('inf'), float('inf'),
+                           float('inf'), float('inf'), float('inf'), 0, 1, 4],
+                          [float('inf'), float('inf'), float('inf'),
+                           float('inf'), float('inf'), float('inf'),
+                           float('inf'), 0, 3],
+                          [float('inf'), float('inf'), float('inf'),
+                           float('inf'), float('inf'), float('inf'),
+                           float('inf'), float('inf'), 0]])
+
+    def test_floyd_warshall_negative_weight_cycle(self):
+        graph = Algorithm(vertices=3)
+        graph.connect_vertices(0, 1, 4)
+        graph.connect_vertices(1, 2, 7)
+        graph.connect_vertices(2, 0, -15)
+        with self.assertRaises(ValueError):
+            graph.floyd_warshall()
